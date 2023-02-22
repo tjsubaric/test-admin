b05bb5c0a92e8385bec65d1caeb086b5dcba0e7d
tjsubaric <91640793+tsubaric@users.noreply.github.com>
2023-02-22 17:26:24 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+// in src/App.tsx
+import { Admin, Resource} from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+import { Dashboard } from './Dashboard';
+import { authProvider } from './authProvider';
+
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => (
+  <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard}>
+     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+     <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation= "name" />
+   </Admin>
+  );
+
+export default App;
